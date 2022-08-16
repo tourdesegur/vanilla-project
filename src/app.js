cc221ed81983ef7b204b92ee067b1bae2f54b080
tourdesegur <106815001+tourdesegur@users.noreply.github.com>
2022-08-16 12:40:13 +0100

    Added Axios
    showing current weather conditions

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-//alert("Hello!");
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "f8620effd7466dda0c3530a3be2c5f5e";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
