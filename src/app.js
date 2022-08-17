dfaea7eea69cb56354bbe310fb583f82291ea569
tourdesegur <106815001+tourdesegur@users.noreply.github.com>
2022-08-17 22:05:44 +0100

    Display forecast data

--- src/app.js
+++ src/app.js
@@ -85,22 +85,39 @@ function displayCelTemperature(event) {
   temperatureElement.innerHTML = Math.round(celTemperature);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
   console.log(response.data);
+  let forecastDaily = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
+
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
-              <div class="forecast-day">${day}</div>
-              <img src="https://ssl.gstatic.com/onebox/weather/48/cloudy.png" />
+  forecastDaily.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+              <div class="forecast-day">${formatDay(forecastDay.dt)}</div>
+              <img src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png" />
               <div class="forecast-temp">
-                <span class="ftemp-max"> 25°</span>
-                <span class="ftemp-min">17°</span>
+                <span class="ftemp-max">${Math.round(
+                  forecastDay.temp.max
+                )}°</span>
+                <span class="ftemp-min">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
               </div>
             </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
